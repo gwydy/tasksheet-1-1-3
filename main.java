272b5aab27e43aa121274fa259c200e9d9f77ff9
gwydy <154076437+gwydy@users.noreply.github.com>
2024-02-09 16:52:01 +0000

    Done

---
+++ main.java
@@ -0,0 +1,22 @@
+public class main{
+
+    public static void main(String[] args) {
+
+        int i;
+
+        for (i = 1; i<=10; i++){
+            if (i%2==0){
+                System.out.println(i + " is even number");
+            }
+            else {
+                System.out.println(i + " is odd number");
+            }
+        }
+        
+
+        //message = (i%2==0) ? i+' is even number' : i+' is odd number';
+
+
+    }
+
+}
